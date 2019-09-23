582c686ffedadf1b07ede399d8af1fea3c602559
ryan <ryan@imac.sh>
2019-09-23 14:56:23 +0800

    add Storage Test

---
+++ src/wallet/contract/StorageTest.js
@@ -0,0 +1,37 @@
+
+import Contract from './Contract'
+
+export default class StorageTest {
+    contractAddress = '0x63a4ae5490aac5aa130f993e86f5357547f9c647ea'
+    // 对应 protocol demo下的Storage合约
+    contract;
+
+    constructor(){
+        this.contract = new Token(this.contractAddress)
+    }
+
+    async testInsert() {
+        let abiInfo = {
+        "constant": false,
+        "inputs": [{
+            "components": [{
+            "name": "text",
+            "type": "string"
+            }],
+            "name": "recordData",
+            "type": "tuple"
+        }],
+        "name": "Insert",
+        "outputs": [{
+            "name": "success",
+            "type": "bool"
+        }],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+        }
+        
+      return this.contract.callContract(abiInfo)
+    }
+  
+  }
--- src/wallet/contract/TokenTest.js
+++ src/wallet/contract/TokenTest.js
@@ -1,19 +1,19 @@
 import Token from './Token'
 
 export default class TokenTest {
-    contractAddress = '0x633c6e19bdc52f8d76baaaa5adc963b09a11a8509c';
+    contractAddress = '0x633c6e19bdc52f8d76baaaa5adc963b09a11a8509c'
 
     /**
      * Addresses of Asilink
      */
-    addr1 = '0x6699fe56a98aa190bdd63239e82d03ae0dba8ad1a1';
-    addr2 = '0x661743c23467045cde4b199a29568dabdb9733a739';
+    addr1 = '0x6699fe56a98aa190bdd63239e82d03ae0dba8ad1a1'
+    addr2 = '0x661743c23467045cde4b199a29568dabdb9733a739'
 
     /**
      * Address of Wallet
      */
-    addr0 = '0x6619fd2d2fd1db189c075ff25800f7b98ff3205e5a';
-    word0 = 'benefit park visit oxygen supply oil pupil snack pipe decade young bracket';
+    addr0 = '0x6619fd2d2fd1db189c075ff25800f7b98ff3205e5a'
+    word0 = 'benefit park visit oxygen supply oil pupil snack pipe decade young bracket'
 
     erc20;
 
