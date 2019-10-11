45500412cd639c48a9d4702a4b29cbae2a1d3c1a
ryan <ryan@imac.sh>
2019-10-11 14:01:55 +0800

    change sleep to settimeout

--- src/adex/cli/watcher.js
+++ src/adex/cli/watcher.js
@@ -5,64 +5,64 @@ import to from 'await-to-js'
 const crypto = require('crypto');
 var date = require("silly-datetime");
 
-export default class watcher{
-    db;
-    exchange;
-    root_hash;
-    constructor() {
-         this.db =  new client();
-         this.utils = new utils2;
-    }
+export default class watcher {
+	db;
+	exchange;
+	root_hash;
+	constructor() {
+		this.db = new client();
+		this.utils = new utils2;
+	}
 
 	async start() {
-		console.log("sssssssssss");
-		 let transactions = await this.db.list_successful_transactions();
+		this.loop()
+	}
 
-            console.log("transactions=",transactions);
-            let id = 0;
+	async loop() {
+		let transactions = await this.db.list_successful_transactions()
 
-            if(transactions.length != 0){
-                id = transactions[0].id;
-            }
+		let id = 0
 
-		while(true){
-			
-			let id_arr = [id+1];
-		 	let transaction = await this.db.get_transaction(id_arr);
-		
-		//全部都是成功的,就睡眠1s
-			 if(transaction.length == 0){
-
-            console.log("have not pending transaction");
-				 this.utils.sleep(1000);
-                continue;
-            }
-			
-			 let [err,result] = await to(chain.getrawtransaction([transaction[0].transaction_hash,true,true]))
-			
-  //          console.log("chain.getrawtransaction",result,err);
-  			//检测txid失败之后，更新为failed就不再管了,暂定为8个区块确认
-			let update_time = this.utils.get_current_time();
-			if(!err && result.confirmations >= 8){
-				let status = 'successful';
-				let info = [status,update_time,id+1]
-				this.db.update_transactions(info);
-				this.db.update_trades(info);
-				id++;
-			}else if(err){
-				let status = 'failed';
-				let info = [status,update_time,id+1]
-				this.db.update_transactions(info);
-				this.db.update_trades(info);
-				id++;
-            	console.log("chain.getrawtransaction--err",err);
-			}else{
-					
-            	console.log("pending transaction",transaction[0].transaction_hash);
-			}
-			this.utils.sleep(1000);
+		if (transactions.length != 0) {
+			id = transactions[0].id;
 		}
-    }
- 
- 
+
+		let id_arr = [id + 1];
+		let transaction = await this.db.get_transaction(id_arr);
+
+		//全部都是成功的,就睡眠1s
+		if (transaction.length == 0) {
+			console.log("have not pending transaction");
+			setTimeout(loop, 1000);
+			continue;
+		}
+
+		let [err, result] = await to(chain.getrawtransaction([transaction[0].transaction_hash, true, true]))
+
+		//          console.log("chain.getrawtransaction",result,err);
+		//检测txid失败之后，更新为failed就不再管了,暂定为8个区块确认
+		let update_time = this.utils.get_current_time();
+		if (!err && result.confirmations >= 8) {
+			let status = 'successful';
+			let info = [status, update_time, id + 1]
+			this.db.update_transactions(info);
+			this.db.update_trades(info);
+			id++;
+		} else if (err) {
+			let status = 'failed';
+			let info = [status, update_time, id + 1]
+			this.db.update_transactions(info);
+			this.db.update_trades(info);
+			id++;
+			console.log("chain.getrawtransaction--err", err);
+		} else {
+
+			console.log("pending transaction", transaction[0].transaction_hash);
+		}
+
+		setTimeout(loop, 1000);
+
+	}
+
+
 }
