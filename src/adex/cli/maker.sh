f8ef29c8908fe964d39f0506a9f0a4d520593dd1
eddy <jiaoguoxingyun@gmail.com>
2019-10-23 13:06:55 +0800

    更新did签名后的刷单脚本

--- src/adex/cli/maker.sh
+++ src/adex/cli/maker.sh
@@ -3,32 +3,74 @@
 #!/bin/bash
 
 #curl -v http://119.23.181.166:13000/adex/list_trades
-i=3;
+i=1;
 markets_arr=("ASIM-PI" "USDT-PI" "MT-PI"  "ASIM-USDT" "ETH-USDT" "BTC-MT" "ETH-MT")
-while((1));
+#markets_arr=("ASIM-PI")
+account_arr=("13682471710" "13682471711")
+address_arr=("0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9" "0x66b7637198aee4fffa103fc0082e7a093f81e05a64")
+side_arr=("buy" "sell")
+index_arr=(1 2)
+while((i--));
 do
 	for marketID in ${markets_arr[@]}
 	do
-	price=$(($(($RANDOM % 50)) + 1));
-	amount=$(($(($RANDOM % 20)) + 1));
-	curl -v http://119.23.181.166:13000/adex/build_order?marketID=$marketID\&side="buy"\&price=${price}\&amount=${amount}\&trader_address="0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9";
-	sleep 50;
+		price=$(($(($RANDOM % 50)) + 1));
+		amount=$(($(($RANDOM % 20)) + 1));
+		order_id=`curl -v http://119.23.181.166:14000/adex/get_order_id?marketID=$marketID\&side="buy"\&price=${price}\&amount=${amount}\&trader_address="0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9" | awk -F '"' '{print $2}'`;
+		echo "order_id=====$order_id";
+		sign=`curl http://119.23.181.166:14000/did/order_sign -H "Accept: application/json"  -H "Content-Type: application/json"  -d '{"username":"13682471710","order_id":"'${order_id}'"}' | jq '[.signature.r,  .signature.s, .signature.pubkey]' | awk -F '"' '{print $2}' `;
+		echo "ssssssssss--${sign}";
+		r=`echo ${sign} | awk -F ' ' '{print $1}' `
+		s=`echo ${sign} | awk -F ' ' '{print $2}' `
+		pubkey=`echo ${sign} | awk -F ' ' '{print $3}' `
+		curl -v http://119.23.181.166:14000/adex/build_order?marketID=$marketID\&side="buy"\&price=${price}\&amount=${amount}\&trader_address="0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9"\&signature="\{\"r\":\"${r}\",\"s\":\"${s}\",\"pubkey\":\"${pubkey}\"\}"\&order_id=${order_id}
+		echo -e "==================\n\n\n\n";
+		sleep 20;
 
-	price2=$(($(($RANDOM % 50)) + 1));
-	amount2=$(($(($RANDOM % 20)) + 1));
-	curl -v http://119.23.181.166:13000/adex/build_order?marketID=$marketID\&side="sell"\&price=${price2}\&amount=${amount2}\&trader_address="0x66b7637198aee4fffa103fc0082e7a093f81e05a64";
-	sleep 50;
 
-	price4=$(($(($RANDOM % 50)) + 1));
-	amount4=$(($(($RANDOM % 20)) + 1));
-	curl -v http://119.23.181.166:13000/adex/build_order?marketID=$marketID\&side="sell"\&price=${price2}\&amount=${amount2}\&trader_address="0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9";
-	sleep 50;
+		price=$(($(($RANDOM % 50)) + 1));
+		amount=$(($(($RANDOM % 20)) + 1));
+		order_id=`curl -v http://119.23.181.166:14000/adex/get_order_id?marketID=$marketID\&side="sell"\&price=${price}\&amount=${amount}\&trader_address="0x66b7637198aee4fffa103fc0082e7a093f81e05a64" | awk -F '"' '{print $2}'`;
+		echo "order_id=====$order_id1";
+		sign=`curl http://119.23.181.166:14000/did/order_sign -H "Accept: application/json"  -H "Content-Type: application/json"  -d '{"username":"13682471711","order_id":"'${order_id}'"}' | jq '[.signature.r,  .signature.s, .signature.pubkey]' | awk -F '"' '{print $2}' `;
+		echo "ssssssssss--${sign}";
+		r=`echo ${sign} | awk -F ' ' '{print $1}' `
+		s=`echo ${sign} | awk -F ' ' '{print $2}' `
+		pubkey=`echo ${sign} | awk -F ' ' '{print $3}' `
+		curl -v http://119.23.181.166:14000/adex/build_order?marketID=$marketID\&side="sell"\&price=${price}\&amount=${amount}\&trader_address="0x66b7637198aee4fffa103fc0082e7a093f81e05a64"\&signature="\{\"r\":\"${r}\",\"s\":\"${s}\",\"pubkey\":\"${pubkey}\"\}"\&order_id=${order_id}
+		echo -e "==================\n\n\n\n";
+		sleep 20;
 
-	price3=$(($(($RANDOM % 50)) + 1));
-	amount3=$(($(($RANDOM % 20)) + 1));
-	curl -v http://119.23.181.166:13000/adex/build_order?marketID=$marketID\&side="buy"\&price=${price}\&amount=${amount}\&trader_address="0x66b7637198aee4fffa103fc0082e7a093f81e05a64";
-	sleep 50;
 
+
+		price=$(($(($RANDOM % 50)) + 1));
+		amount=$(($(($RANDOM % 20)) + 1));
+		order_id=`curl -v http://119.23.181.166:14000/adex/get_order_id?marketID=$marketID\&side="sell"\&price=${price}\&amount=${amount}\&trader_address="0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9" | awk -F '"' '{print $2}'`;
+		echo "order_id=====$order_id";
+		sign=`curl http://119.23.181.166:14000/did/order_sign -H "Accept: application/json"  -H "Content-Type: application/json"  -d '{"username":"13682471710","order_id":"'${order_id}'"}' | jq '[.signature.r,  .signature.s, .signature.pubkey]' | awk -F '"' '{print $2}' `;
+		echo "ssssssssss--${sign}";
+		r=`echo ${sign} | awk -F ' ' '{print $1}' `
+		s=`echo ${sign} | awk -F ' ' '{print $2}' `
+		pubkey=`echo ${sign} | awk -F ' ' '{print $3}' `
+		curl -v http://119.23.181.166:14000/adex/build_order?marketID=$marketID\&side="sell"\&price=${price}\&amount=${amount}\&trader_address="0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9"\&signature="\{\"r\":\"${r}\",\"s\":\"${s}\",\"pubkey\":\"${pubkey}\"\}"\&order_id=${order_id}
+		echo -e "==================\n\n\n\n";
+		sleep 20;
+
+
+
+
+		price=$(($(($RANDOM % 50)) + 1));
+		amount=$(($(($RANDOM % 20)) + 1));
+		order_id=`curl -v http://119.23.181.166:14000/adex/get_order_id?marketID=$marketID\&side="buy"\&price=${price}\&amount=${amount}\&trader_address="0x66b7637198aee4fffa103fc0082e7a093f81e05a64" | awk -F '"' '{print $2}'`;
+		echo "order_id=====$order_id";
+		sign=`curl http://119.23.181.166:14000/did/order_sign -H "Accept: application/json"  -H "Content-Type: application/json"  -d '{"username":"13682471711","order_id":"'${order_id}'"}' | jq '[.signature.r,  .signature.s, .signature.pubkey]' | awk -F '"' '{print $2}' `;
+		echo "ssssssssss--${sign}";
+		r=`echo ${sign} | awk -F ' ' '{print $1}' `
+		s=`echo ${sign} | awk -F ' ' '{print $2}' `
+		pubkey=`echo ${sign} | awk -F ' ' '{print $3}' `
+		curl -v http://119.23.181.166:14000/adex/build_order?marketID=$marketID\&side="buy"\&price=${price}\&amount=${amount}\&trader_address="0x66b7637198aee4fffa103fc0082e7a093f81e05a64"\&signature="\{\"r\":\"${r}\",\"s\":\"${s}\",\"pubkey\":\"${pubkey}\"\}"\&order_id=${order_id}
+		echo -e "==================\n\n\n\n";
+		sleep 20;
 
 	done
 done
