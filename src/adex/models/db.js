6495a67007d10ed6efbace286b8ded1ea0bc4f69
eddy <jiaoguoxingyun@gmail.com>
2019-11-12 21:39:28 +0800

    区分卖单和买单的orderbook排序

--- src/adex/models/db.js
+++ src/adex/models/db.js
@@ -119,13 +119,20 @@ export default class db{
 
 
 
-        async order_book(filter_info) {
-			let [err,result] = await to(this.clientDB.query('select s.* from  (SELECT price,sum(available_amount) as amount FROM mist_orders\
-			where available_amount>0  and side=$1 and market_id=$2 group by price)s order by s.price desc limit 100',filter_info)); 
-			if(err) {
-				return console.error('order_book_查询失败', err);
+        async order_book(filter) {
+			let err;
+			let result;
+			if(filter[1] == 'sell'){
+				[err,result] = await to(this.clientDB.query('select s.* from  (SELECT price,sum(available_amount) as amount FROM mist_orders\
+            where available_amount>0  and side=$1 and market_id=$2 group by price)s order by s.price asc limit 100',filter)); 
+			}else{
+				
+				[err,result] = await to(this.clientDB.query('select s.* from  (SELECT price,sum(available_amount) as amount FROM mist_orders\
+            where available_amount>0  and side=$1 and market_id=$2 group by price)s order by s.price desc limit 100',filter)); 
 			}
-			//console.log('list_order_成功',JSON.stringify(result.rows)); 
+			if(err) {
+				return console.error('filter_orders_查询失败11',filter);
+			}
 			return result.rows;
         }
 
