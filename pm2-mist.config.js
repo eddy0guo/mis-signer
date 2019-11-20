69fdd01024c212f4ccc95d6cbfe9e4afbe044727
ryan <ryan@imac.sh>
2019-11-20 22:30:49 +0800

    一次启动2个环境

--- pm2-mist.config.js
+++ pm2-mist.config.js
@@ -1,6 +1,7 @@
 // npm run build
 // pm2 start pm2-mist.config.js --only mist --env production
-// pm2 start pm2-mist.config.js --only mist --env env_poa_production
+// pm2 start pm2-mist.config.js --only mist-poa --env production
+// pm2 start pm2-mist.config.js --env production
 
 module.exports = {
   apps: [{
@@ -13,12 +14,68 @@ module.exports = {
     env_production: {
       NODE_ENV: "production",
       MIST_MODE: "postgres",
+    }
+  },
+  {
+    name: "mist-bot",
+    script: "./dist/bot",
+    env: {
+      NODE_ENV: "development",
+      MIST_MODE: "mist_test",
     },
-    env_poa_production: {
+    env_production: {
+      NODE_ENV: "production",
+      MIST_MODE: "postgres",
+    }
+  },
+  {
+    name: "mist",
+    script: "./dist/ws",
+    env: {
+      NODE_ENV: "development",
+      MIST_MODE: "mist_test",
+    },
+    env_production: {
+      NODE_ENV: "production",
+      MIST_MODE: "postgres",
+    }
+  },
+  {
+    name: "mist-poa",
+    script: "./dist",
+    env: {
+      NODE_ENV: "development",
+      MIST_MODE: "mist_test",
+    },
+    env_production: {
       NODE_ENV: "production",
       MIST_MODE: "mist_hqtn",
     }
-  }
+  },
+  {
+    name: "mist-poa-bot",
+    script: "./dist/bot",
+    env: {
+      NODE_ENV: "development",
+      MIST_MODE: "mist_test",
+    },
+    env_production: {
+      NODE_ENV: "production",
+      MIST_MODE: "mist_hqtn",
+    }
+  },
+  {
+    name: "mist-poa-ws",
+    script: "./dist/ws",
+    env: {
+      NODE_ENV: "development",
+      MIST_MODE: "mist_test",
+    },
+    env_production: {
+      NODE_ENV: "production",
+      MIST_MODE: "mist_hqtn",
+    }
+  },
     // {
     //   name: 'bot',
     //   script: './dist/bot'
