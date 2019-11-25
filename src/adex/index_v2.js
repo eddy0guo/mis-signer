05210b8c661183b11579062ca7202c6b3dca3ddf
eddy <jiaoguoxingyun@gmail.com>
2019-11-25 17:03:15 +0800

    规范的接口先放在v2后续进行对接，目前先回退老接口

--- src/adex/index_v2.js
+++ src/adex/index_v2.js
@@ -47,121 +47,172 @@ export default ({ config, db,logger}) => {
 	launcher.start();
 
 	adex.get('/mist_engine_info', async (req, res) => {
-					 let result = await trades.get_engine_info();
-                    console.log(result)
-       res.json({result});
+		let [err,result] = await to(trades.get_engine_info());
+		console.log(result)
+
+		res.json({
+			success: result == undefined ? false:true,
+			result: result,
+			err:err
+		});
 	});
 
 
 	        
    	adex.get('/list_market_quotations', async (req, res) => {
-					 let result = await market.list_market_quotations();
-                    console.log(result)
-       res.json({result});
+		let [err,result] = await to(market.list_market_quotations());
+		res.json({
+			success: result == undefined ? false:true,
+			result: result,
+			err:err
+		});
 	});
 
 adex.get('/list_tokens', async (req, res) => {
-					 let result = await mist_wallet.list_tokens();
-                    console.log(result)
-       res.json({result});
+		let [err,result] = await to(mist_wallet.list_tokens());
+		res.json({
+			success: result == undefined ? false:true,
+			result: result,
+			err:err
+		});
 	});
 
 
 
 
  	adex.get('/get_token_price', async (req, res) => {
-				var obj = urllib.parse(req.url,true).query;
- 	 				  console.log("obj=",obj);
-				let result = await mist_wallet.get_token_price2pi(obj.symbol);
-                    console.log(result);
+		var obj = urllib.parse(req.url,true).query;
+		let [err,result] = await to(mist_wallet.get_token_price2pi(obj.symbol));
+		res.json({
+			success: result == undefined ? false:true,
+			result: result,
+			err:err
+		});
 
-
-       res.json({result});
 	});
 
 	adex.get('/get_token_price2btc', async (req, res) => {
-			var obj = urllib.parse(req.url,true).query;
- 	 				  console.log("obj=",obj);
-			let result = await mist_wallet.get_token_price2btc(obj.symbol);
-                    console.log(result);
-
-
-       res.json({result});
+		var obj = urllib.parse(req.url,true).query;
+		let [err,result] = await to(mist_wallet.get_token_price2btc(obj.symbol));
+		res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 
 
 
     adex.get('/balances',async (req, res) => {
-					var obj = urllib.parse(req.url,true).query;
- 	 				  console.log("obj=",obj);
-                    let token_arr = await mist_wallet.list_tokens();
-					let balances = [];
- 	 				  console.log("obj11111111133=",token_arr);
-                    for(var i in token_arr){
-                    	    let token = new Token(token_arr[i].address);
-                            let [err,result] = await to(token.balanceOf(obj.address));
-							console.log("444444444",mist_config);
-							let [err3,allowance] = await to(token.allowance(obj.address,mist_config.ex_address));
+		var obj = urllib.parse(req.url,true).query;
+		let [err,token_arr] = await to(mist_wallet.list_tokens());
+		if(err){
+			return res.json({
+					success: false,
+					err:err
+					});
+		}
 
-							let asset = new Asset(token_arr[i].asim_assetid)
-        					let [err4,assets_balance] = await to(asset.balanceOf(obj.address))
-							let asset_balance=0;
-							for(let j in assets_balance){
-								if( token_arr[i].asim_assetid == assets_balance[j].asset){
-									asset_balance = assets_balance[j].value;	
-								}
-							}
+		let balances = [];
+		for(var i in token_arr){
+				let token = new Token(token_arr[i].address);
+				let [err2,result] = await to(token.balanceOf(obj.address));
+				if(err2){
+					return res.json({
+							success: false,
+							err:err2
+							});
+				}
 
-							let balance_info ={
-								token_symbol: token_arr[i].symbol,   
-								token_name: token_arr[i].name,   
-								balance:result / (1 * 10 ** 8),
-								allowance_ex:allowance / (1 * 10 ** 8),
-								asim_assetid: token_arr[i].asim_assetid,
-								asim_asset_balance: asset_balance
-							};
-
- 	 				  		console.log("obj111111111=",token_arr[i]);
-							balances.push(balance_info);
-                            console.log(balance_info);
-                    }
-
-                    res.json(balances);
-                    });
+				console.log("444444444",mist_config);
+				let [err3,allowance] = await to(token.allowance(obj.address,mist_config.ex_address));
+				if(err3){
+					return res.json({
+							success: false,
+							err:err
+							});
+				}
 
 
-    //所有token合约赋予所有地址权限
+				let asset = new Asset(token_arr[i].asim_assetid)
+				let [err4,assets_balance] = await to(asset.balanceOf(obj.address))
+				if(err4){
+					return res.json({
+							success: false,
+							err:err4
+							});
+				}
+
+				let asset_balance=0;
+				for(let j in assets_balance){
+					if( token_arr[i].asim_assetid == assets_balance[j].asset){
+						asset_balance = assets_balance[j].value;	
+					}
+				}
+
+				let balance_info ={
+					token_symbol: token_arr[i].symbol,   
+					token_name: token_arr[i].name,   
+					balance:result / (1 * 10 ** 8),
+					allowance_ex:allowance / (1 * 10 ** 8),
+					asim_assetid: token_arr[i].asim_assetid,
+					asim_asset_balance: asset_balance
+				};
+
+				console.log("obj111111111=",token_arr[i]);
+				balances.push(balance_info);
+				console.log(balance_info);
+		}
+
+		res.json({success: true,result: balances});
+		 //res.json(balances);
+
+
+
+    });
+
+
+    //所有token合约赋予所有地址权限(弃用，改为did签名，后台广播)
     adex.get('/approves',async (req, res) => {
                     
-                   var obj = urllib.parse(req.url,true).query;
-                      console.log("obj=",obj);
-					let token_arr = await mist_wallet.list_tokens();
-                    let txids =[];
-                    for(let i in token_arr){
-                                    let token  = new Token(token_arr[i].address);
-                                    let wallet = await my_wallet(obj.word);
-                                    let address = await wallet.getAddress();
+		   var obj = urllib.parse(req.url,true).query;
+			let [err,token_arr] = await to(mist_wallet.list_tokens());
+			if(err){
+				return res.json({
+						success: false,
+						err:err
+						});
+			}
+			let txids =[];
+			for(let i in token_arr){
+					let token  = new Token(token_arr[i].address);
+					let wallet = await my_wallet(obj.word);
+					let address = await wallet.getAddress();
+					
+					 token.unlock(wallet,"111111")
+				   let [err1,balance] = await to(token.balanceOf(address));
+					let [err2,allowance] = await to(token.allowance(address,mist_config.ex_address));
+					if(balance != allowance){
+						await wallet.queryAllBalance()
+						let [err3,txid] = await to(token.approve(mist_config.ex_address,9999999));
+						console.log("333--address---",err2,txid);
+						if(err3){
+						return res.json({
+								success: false,
+								err: err3
+								});
+						}
 
-                                    console.log("333--address",address);
-                                    
-                                     token.unlock(wallet,"111111")
-                                   let [err,balance] = await to(token.balanceOf(address));
-                                    let [err3,allowance] = await to(token.allowance(address,mist_config.ex_address));
-                                    if(balance != allowance){
-                                        await wallet.queryAllBalance()
-                                        let [err2,txid] = await to(token.approve(mist_config.ex_address,9999999));
+						txids.push(txid);
+					}
+			}
 
-                                    console.log("333--address---",err2,txid);
-
-                                        txids.push(txid);
-                                        console.log("444--",err2,txid);
-
-                                    }
-                                    console.log("444--",balance,allowance);
-                    }
-                    res.json(txids);
-                    });
+		res.json({
+				success: true,
+				result: txids
+		});
+	});
  	/****
 	
 get_order_id，获取order_id,
@@ -190,8 +241,7 @@ did对order_id进行签名，获取rsv
                       created_at:null,
        };
 	   let order_id = utils.get_hash(message);
-
-       res.json(order_id);
+		res.json({success: true,result: order_id});
 	});
 
 
@@ -205,19 +255,21 @@ did对order_id进行签名，获取rsv
 	  /** 
 		let result = utils.verify(obj.order_id,JSON.parse(obj.signature));
 		if(!result){
-			return res.json("verify failed");
+
+			return res.json({success: false,err: "verify failed"});
 		}
 		**/
 		if(!(utils.judge_legal_num(+obj.amount) && utils.judge_legal_num(+obj.price))){
-			return res.json("amount or price is cannt support");
+			return res.json({success: false,err: "amount or price is cannt support"});
 		}
+		//还得加上超卖的判断fixme
 		/*
 		var arr = obj.market.toString().split("-");
 		let token_info = mist_wallet.get_token(arr[1]);
 		let token = new Token(token_info[0].address);
         let balance = await token.balanceOf(obj.address);
 		if(NP.times(+obj.amount, +obj.price) > balance){
-			return res.json("balance is not enoungh");
+			return res.json({success: false,err: "balance is not enoungh"});
 		}
 		*/
 
@@ -240,9 +292,13 @@ did对order_id进行签名，获取rsv
        };
 
 
-       let [err,result2] = await to(order.build(message))
-       console.log(result2,err);
-       res.json({ result2,err });
+       let [err,result] = await to(order.build(message));
+       console.log(result,err);
+		res.json({
+			success: result == undefined ? false:true,
+			result: result,
+			err:err
+		});
 	});
 
 	adex.get('/cancle_order', async (req, res) => {
@@ -255,43 +311,50 @@ did对order_id进行签名，获取rsv
 
 
        let [err,result] = await to(order.cancle_order(message));
-       res.json({result,err });
+		res.json({
+			success: result == undefined ? false:true,
+			result: result,
+			err:err
+		});
 	});
 
-	//撤销订单，应该是需要对每个orderid进行签名，后期做
-    adex.get('/cancle_my_order/:address', async (req, res) => {
+//撤销订单，应该是需要对每个orderid进行签名，后期做
+	adex.get('/cancle_my_order/:address', async (req, res) => {
+	   
+		//暂时只支持取消1000以内的单子
+		let [err,orders] = await to(order.my_orders2(req.params.address,1,1000,'pending','partial_filled'));
+		console.log("cancle_my_order=",orders,err,req.params.address);
 
-        //暂时只支持取消1000以内的单子
-        let [err,orders] = await to(order.my_orders2(req.params.address,1,1000,'pending','partial_filled'));
-        console.log("cancle_my_order=",orders,err,req.params.address);
+		if(!err){
+			for(let index in orders){
+				let message = {amount:orders[index].available_amount,id:orders[index].id};
+				console.log("cancle_my_order",message)
 
-        if(!err){
-            for(let index in orders){
-                let message = {amount:orders[index].available_amount,id:orders[index].id};
-                console.log("cancle_my_order",message)
+			   let [err,result] = await to(order.cancle_order(message));
+				if(err){
+					return res.json({
+						success: false,
+						err:err
+					})
+				}
+			}
+		}
 
-               let [err,result] = await to(order.cancle_order(message));
-                if(err){
-                    return res.json({
-                        success: false,
-                        err:err
-                    })
-                }
-            }
-        }
-
-        res.json({
-            success: true,
-        });
-    });
+		res.json({
+			success: true,
+		});
+	});
 
 
 	adex.get('/list_orders', async (req, res) => {
        
 
        let [err,result] = await to(order.list_orders());
-
-       res.json({result,err });
+	   res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 	adex.get('/my_orders', async (req, res) => {
@@ -304,8 +367,11 @@ did对order_id进行签名，获取rsv
 		let message = {address:obj.address}
 
        let [err,result] = await to(order.my_orders(message));
-
-       res.json({result,err });
+	   res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 	adex.get('/my_orders2/:address/:page/:perpage/:status1/:status2', async (req, res) => {
@@ -313,7 +379,11 @@ did对order_id进行签名，获取rsv
 		//cancled，full_filled，历史委托
 	   let {address,page,perpage,status1,status2} = req.params;
        let [err,result] = await to(order.my_orders2(address,page,perpage,status1,status2));
-       res.json({result,err });
+	   res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 
@@ -326,13 +396,21 @@ did对order_id进行签名，获取rsv
        console.log("obj=",obj);
 	
        let [err,result] = await to(order.order_book(obj.marketID));
-       res.json({result,err });
+	   res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 	adex.get('/list_markets', async (req, res) => {
 
        let [err,result] = await to(market.list_markets());
-       res.json({result,err });
+	   res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 
@@ -343,8 +421,11 @@ did对order_id进行签名，获取rsv
        console.log("obj=",obj);
 
        let [err,result] = await to(trades.rollback_trades());
-
-       res.json({result,err });
+		res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 
@@ -356,28 +437,36 @@ did对order_id进行签名，获取rsv
        console.log("obj=",obj);
 
        let [err,result] = await to(trades.list_trades(obj.marketID));
+	   res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 
-       res.json({result,err });
 	});
 
 
     adex.get('/my_trades', async (req, res) => {
-      /**
-        let message = {address:"0x66b7637198aee4fffa103fc0082e7a093f81e05a64"}
-**/
         var obj = urllib.parse(req.url,true).query;
        console.log("obj=",obj);
         let message = {address:obj.address};
        let [err,result] = await to(trades.my_trades(message));
+		 res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 
-       res.json({result,err });
     });
 
 
 	adex.get('/my_trades2/address/page/perpage', async (req, res) => {
        let [err,result] = await to(trades.my_trades2(req.params.address,req.params.page,req.params.per_page));
-
-       res.json({result,err });
+	     res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 
@@ -385,29 +474,20 @@ did对order_id进行签名，获取rsv
 	// add 10 second memory cache ( change to redis later )
 	adex.get('/trading_view',cache('10 second'), async (req, res) => {
 		let current_time = Math.floor(new Date().getTime() / 1000);
-/**
-		let message = {
-		market_id:"ASIM-PAI",   
-		from: current_time - current_time%300 - 300*100,   //当前所在的时间区间不计算  
-		to: current_time - current_time%300,
-		granularity: 300,
-		};
-**/
-
-		 var obj = urllib.parse(req.url,true).query;
-       console.log("obj=",obj);
-
-
+		var obj = urllib.parse(req.url,true).query;
+        console.log("obj=",obj);
 		let message = {
 		market_id:obj.marketID,   
 		from: current_time - current_time%obj.granularity - obj.granularity*obj.number,   //当前所在的时间区间不计算  
 		to: current_time - current_time%obj.granularity,
 		granularity: obj.granularity,
 		};
-	   
-
 		let [err,result] = await to(trades.trading_view(message));
-        res.json({result,err });
+		res.json({
+            success: result == undefined ? false:true,
+            result: result,
+            err:err
+        });
 	});
 
 	return adex;
