792ba4591b27c6a1244f28bca2afee6f3c2d2262
eddy <jiaoguoxingyun@gmail.com>
2019-11-27 16:06:57 +0800

    更换邮箱

--- src/did/models/mail.js
+++ src/did/models/mail.js
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
-    service: 'qq', // qq,网易
+    service: 'Zoho', // qq,网易
     port: 465,
     secure: true, // true for 465, false for other ports
     auth: {
-        user: "792130043@qq.com",
-        pass: 'vsvnfrtignzcbcgd',
+        user: "code@fingo.com",
+        pass: 'hongqiaolvgu',
     }
 });
 
@@ -13,11 +13,11 @@ let mail = {
     transporter: transporter,
     send(mail, content, callback) {
         let mailOptions = {
-            from: '"MIST EXCHANGE" <792130043@qq.com>', 
+            from: '"fingo" <code@fingo.com>', 
             to: mail, // list of receivers
             subject: '获取验证码', // Subject line
             text: `${content}`, // plain text body
-            html: `验证码为:${content},有效期为五分钟.` // html body   
+            html: `验证码为:${content},有效期为十五分钟.` // html body   
         }
 
         this.transporter.sendMail(mailOptions, (error, info) => {
