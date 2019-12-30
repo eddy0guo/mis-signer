054a6a2338df5acc164b5771a7b8fe95501914bf
eddy <jiaoguoxingyun@gmail.com>
2019-12-30 18:08:58 +0800

    根据机器人的状况微调队列参数

--- src/adex/api/engine.js
+++ src/adex/api/engine.js
@@ -138,7 +138,7 @@ export default class engine {
 		let matched_trades = await this.db.list_matched_trades();
 		//累计没超过100+1,粒度小，relayer高频，粒度大relayer低频
 		//新加坡服务器访问慢，先调大
-		let add_queue_num  = Math.floor(matched_trades[0].count / 300 ) + 1;
+		let add_queue_num  = Math.floor(matched_trades[0].count / 100 ) + 1;
 
 		let transaction_id = transactions.length == 0 ?  0 : transactions[0].transaction_id + add_queue_num;
 
