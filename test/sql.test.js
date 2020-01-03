ea6a5e7da3dc4e14545dd6e73f476db9efe04f11
airbc <ryan@see.ec>
2020-01-03 19:11:54 +0800

    sql test

--- test/sql.test.js
+++ test/sql.test.js
@@ -1,3 +1,11 @@
-test('adds 1 + 2 to equal 3', () => {
-    expect(1+2).toBe(3);
+import DB from '../src/adex/models/db'
+const db = new DB()
+
+test('adds 1 + 2 to equal 3', async() => {
+    const filter = [3000, 'buy', 'BTC-USDT'];
+    const start = new Date().getTime()
+    const result = await db.filter_orders(filter);
+    const end = new Date().getTime()
+    expect(result).toBeDefined()
+    expect(end-start).toBeGreaterThen(1000)
 });
