e1dae7471302cee9d62924267632bd15d0a3c0a0
eddy <jiaoguoxingyun@gmail.com>
2020-01-18 23:44:26 +0800

    +1

--- src/adex/models/product_conf/seed.sql
+++ src/adex/models/product_conf/seed.sql
@@ -15,14 +15,14 @@ insert into mist_markets (id,base_token_address,base_token_symbol,quote_token_ad
 
 insert into mist_tokens (address, symbol, name, decimals,asim_address,asim_assetid) values ('0x63fc1a4989e656387e351610ccc916dde39a2cfa7e', 'CNYC','CNYCI', 8,'0x63ed080e7f11494e7563fff04668dfddc1555398de','000000000000000600000000');
 insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid) values ('0x6345fba46219b9cf6c62e090e5753df302ebfcf374', 'ASIM','ASIMM', 8,'','000000000000000000000000');
-insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid) values ('0x63086a61a5d0269cb1d6c22a9fc829585fb21fdaa9', 'BTC','BTCC', 8,'0x63ed080e7f11494e7563fff04668dfddc1555398de','000000000000000700000001');
-insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid) values ('0x63e1385fc6493ee85bcd6b17d56c4c690951fee1e0', 'USDT','USDTS', 8,'0x63ed080e7f11494e7563fff04668dfddc1555398de','000000000000000200000003');
+insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid) values ('0x63086a61a5d0269cb1d6c22a9fc829585fb21fdaa9', 'BTC','BTCC', 8,'0x63ed080e7f11494e7563fff04668dfddc1555398de','000000000000000500000001');
+insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid) values ('0x63e1385fc6493ee85bcd6b17d56c4c690951fee1e0', 'USDT','USDTS', 8,'0x63ed080e7f11494e7563fff04668dfddc1555398de','000000000000000500000003');
 insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid) values ('0x639b93ed84ea37ae3ec9ecffe44dcc7c83c44b5a3a', 'ETH','ETHP', 8,'0x63ed080e7f11494e7563fff04668dfddc1555398de','000000000000000500000002');
-insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid values ('0x639eede549711dae12f683486fff42133cb594fc13', 'MT','MTT', 8,'0x632afcb303c312d0e1f4cf429212d005b76a496886','000000000000000700000001');
+insert into mist_tokens (address, symbol, name,decimals,asim_address,asim_assetid) values ('0x639eede549711dae12f683486fff42133cb594fc13', 'MT','MTT', 8,'0x632afcb303c312d0e1f4cf429212d005b76a496886','000000000000000700000001');
 
 insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('CNYC','000000000000000600000000','0x63ed080e7f11494e7563fff04668dfddc1555398de',0,0);
 insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('ASIM','000000000000000000000000','',0,0);
-insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('BTC','000000000000000700000001','0x63ed080e7f11494e7563fff04668dfddc1555398de',0,0);
-insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('USDT','000000000000000200000003','0x63ed080e7f11494e7563fff04668dfddc1555398de',0,0);
+insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('BTC','000000000000000500000001','0x63ed080e7f11494e7563fff04668dfddc1555398de',0,0);
+insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('USDT','000000000000000500000003','0x63ed080e7f11494e7563fff04668dfddc1555398de',0,0);
 insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('ETH','000000000000000500000002','0x63ed080e7f11494e7563fff04668dfddc1555398de',0,0);
 insert into asim_assets_info (asset_name, asset_id, contract_address,total,yesterday_total) values ('MT','000000000000000700000001','0x632afcb303c312d0e1f4cf429212d005b76a496886',0,0);
