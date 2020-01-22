bdc38f5d2be5125f9381b2f3da75300432761724
ryan <ryan@imac.sh>
2020-01-23 00:12:49 +0800

    format

--- src/index.js
+++ src/index.js
@@ -4,26 +4,25 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import wallet from './wallet';
-import adex  from './adex';
+import adex from './adex';
 import config from './config.json';
 import express1 from './express'
 import mist_config from './cfg'
 
-var responseTime = function () {
+const responseTime = function () {
     return function (req, res, next) {
-            req._startTime = new Date() // 获取时间 t1
+        req._startTime = new Date() // 获取时间 t1
 
         var calResponseTime = function () {
-        var now = new Date(); 2
-        var deltaTime = now - req._startTime;
-                console.log(`--deltaTime-----${deltaTime}--------`);
+            var now = new Date();
+            var deltaTime = now - req._startTime;
+            console.log(`[REQ TIME]:${deltaTime}`);
         }
 
         res.once('finish', calResponseTime);
         res.once('close', calResponseTime);
         return next();
-   }
-
+    }
 }
 
 let app = express();
@@ -35,38 +34,36 @@ app.use(morgan('dev'));
 
 // 3rd party middleware
 app.use(cors({
-	exposedHeaders: config.corsHeaders
+    exposedHeaders: config.corsHeaders
 }));
 
 app.use(bodyParser.json({
-	limit : config.bodyLimit
+    limit: config.bodyLimit
 }));
 
-app.all("*",function(req,res,next){
+app.all("*", function (req, res, next) {
     //设置允许跨域的域名，*代表允许任意域名跨域
-    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Origin", "*");
     //允许的header类型
-    res.header("Access-Control-Allow-Headers","content-type");
+    res.header("Access-Control-Allow-Headers", "content-type");
     //跨域允许的请求方式
-    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    res.header("Access-Control-Allow-Methods", "DELETE,PUT,POST,GET,OPTIONS");
     if (req.method.toLowerCase() == 'options')
         res.send(200);  //让options尝试请求快速结束
     else
         next();
 });
 
+// internal middleware
 app.use(responseTime())
 
-
-// internal middleware
-
 // api router
-app.use('/wallet', wallet({ config}));
-app.use('/adex',adex({ config}));
-app.use('/express',express1({config}))
+app.use('/wallet', wallet({ config }));
+app.use('/adex', adex({ config }));
+app.use('/express', express1({ config }))
 
 app.server.listen(process.env.PORT || mist_config.mist_server_port, () => {
-	console.log(`Started on port ${app.server.address().port}`);
+    console.log(`Started on port ${app.server.address().port}`);
 });
 
 
