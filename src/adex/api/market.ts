655e58b4a17b6f1b5e31e09ae5357b38a9d23aff
eddy <jiaoguoxingyun@gmail.com>
2020-02-13 20:54:00 +0800

    增加trades无数据时list_market_quotations的逻辑判断

--- src/adex/api/market.ts
+++ src/adex/api/market.ts
@@ -30,11 +30,22 @@ export default class Market {
         for (const index in markets) {
             if( !markets[index])continue
             const [err, result]: [any,any] = await to(this.db.get_market_quotations([markets[index].id]));
-            if (!err && result && result.length > 0) {
-                const base_token = result[0].market_id.split('-')[0];
+			if (!err && result && result.length > 0) {
+                let base_token = result[0].market_id.split('-')[0];
                 result[0].CNYC_price = await quotation.get_token_price2pi(base_token);
-                quotations.push(result[0]);
-            }
+
+            }else if(!err && result && result.length === 0){
+                result[0] = {
+                    market_id: markets[index].id,
+                    price: 0,
+                    ratio: 0,
+                    volume: 0,
+                    CNYC_price: 0
+                }
+            }else{
+				console.error(err);	
+			}
+            quotations.push(result[0]);
         }
         return quotations;
     }
