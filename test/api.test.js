08d60e872e47e613cd58271d6955d3be17ccbc88
eddy <jiaoguoxingyun@gmail.com>
2020-02-14 22:04:05 +0800

    本地api测试改为生产的域名

--- test/api.test.js
+++ test/api.test.js
@@ -9,28 +9,28 @@ describe('API', function() {
 
 	it('myorders', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/my_orders_v2/0x66a6c15b2a6a253aa2f67ea525b2619f12ae8d3509/1/30/full_filled/partial_filled")
+		let result = await rp("https://api.appdev.fingo.com/adex/my_orders_v2/0x66a6c15b2a6a253aa2f67ea525b2619f12ae8d3509/1/30/full_filled/partial_filled")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 
-	it('myorders', async() =>{
+	it('mytrades', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/my_orders_v2/0x66a6c15b2a6a253aa2f67ea525b2619f12ae8d3509/1/30/full_filled/partial_filled")
+		let result = await rp("https://api.appdev.fingo.com/adex/my_trades_v2/0x66a6c15b2a6a253aa2f67ea525b2619f12ae8d3509/1/30")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('mist_user_overview', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/mist_user_overview/0x66a6c15b2a6a253aa2f67ea525b2619f12ae8d3509")
+		let result = await rp("https://api.appdev.fingo.com/adex/mist_user_overview/0x66a6c15b2a6a253aa2f67ea525b2619f12ae8d3509")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
@@ -40,7 +40,7 @@ describe('API', function() {
 
 	it('list_market_quotations', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/list_market_quotations_v2")
+		let result = await rp("https://api.appdev.fingo.com/adex/list_market_quotations_v2")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.result.length).to.not.equal(0)
@@ -51,17 +51,17 @@ describe('API', function() {
 
 	it('get_token_price', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/get_token_price_v2/BTC")
+		let result = await rp("https://api.appdev.fingo.com/adex/get_token_price_v2/BTC")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('balances', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/balances_v2?address=0x66ea4b7f7ad33b0cc7ef94bef71bc302789b815c46")
+		let result = await rp("https://api.appdev.fingo.com/adex/balances_v2?address=0x66ea4b7f7ad33b0cc7ef94bef71bc302789b815c46")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
@@ -70,7 +70,7 @@ describe('API', function() {
 
 	it('asset_balances', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/asset_balances/0x66ea4b7f7ad33b0cc7ef94bef71bc302789b815c46")
+		let result = await rp("https://api.appdev.fingo.com/adex/asset_balances/0x66ea4b7f7ad33b0cc7ef94bef71bc302789b815c46")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
@@ -80,7 +80,7 @@ describe('API', function() {
 
 	it('erc20_balances', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/erc20_balances/0x66ea4b7f7ad33b0cc7ef94bef71bc302789b815c46")
+		let result = await rp("https://api.appdev.fingo.com/adex/erc20_balances/0x66ea4b7f7ad33b0cc7ef94bef71bc302789b815c46")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
@@ -90,11 +90,11 @@ describe('API', function() {
 
 	it('get_order_id', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/get_order_id_v2/0x66a9ae316e1914dc8d835d5cd2ed57ab24b52a02c7/ASIM-CNYC/sell/100/6000")
+		let result = await rp("https://api.appdev.fingo.com/adex/get_order_id_v2/0x66a9ae316e1914dc8d835d5cd2ed57ab24b52a02c7/ASIM-CNYC/sell/100/6000")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 /*
@@ -102,7 +102,7 @@ describe('API', function() {
 
 		var options = {
 			method: 'POST',
-			uri: 'http://127.0.0.1:21000/adex/build_order_v3',
+			uri: 'https://api.appdev.fingo.com/adex/build_order_v3',
 			body: {
 			signature:{
 					r: '19e54db2a1871c6ea22f4b195598a3f368c5d7b6dd65e89deeb764ccc5782d73',
@@ -123,63 +123,63 @@ describe('API', function() {
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});/
 
 	it('cancle_orders', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/")
+		let result = await rp("https://api.appdev.fingo.com/adex/")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 */
 	it('my_orders', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/my_orders_v2/0x6622bd37c1331b34359920f1eaa18a38ba9ff203e9/1/1/pending/fullfuilled")
+		let result = await rp("https://api.appdev.fingo.com/adex/my_orders_v2/0x6622bd37c1331b34359920f1eaa18a38ba9ff203e9/1/1/pending/fullfuilled")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('my_trades', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/my_trades_v2/0x6622bd37c1331b34359920f1eaa18a38ba9ff203e9/1/1")
+		let result = await rp("https://api.appdev.fingo.com/adex/my_trades_v2/0x6622bd37c1331b34359920f1eaa18a38ba9ff203e9/1/1")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 /*
 	it('asset2coin_v3', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/")
+		let result = await rp("https://api.appdev.fingo.com/adex/")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('coin2asset_v3', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/adex/")
+		let result = await rp("https://api.appdev.fingo.com/adex/")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 */
 	it('burn_coin_tohex', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/wallet/burn_coin_tohex/0x66d130d64c8d988800664616383a2bda48ffd4eecc/CNYC/1")
+		let result = await rp("https://api.appdev.fingo.com/wallet/burn_coin_tohex/0x66d130d64c8d988800664616383a2bda48ffd4eecc/CNYC/1")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
@@ -189,89 +189,89 @@ describe('API', function() {
 
 	it('find_convert', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/wallet/find_convert/8c4ddabebe95718a37aea074120d3bd133196c01812935ddef42dffcdfd431ac")
+		let result = await rp("https://api.appdev.fingo.com/wallet/find_convert/8c4ddabebe95718a37aea074120d3bd133196c01812935ddef42dffcdfd431ac")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('my_converts', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/wallet/my_converts_v3/0x6602ca6e2820ec98cc68909fdd9f87c7bd23b62000/ASIM/1/10")
+		let result = await rp("https://api.appdev.fingo.com/wallet/my_converts_v3/0x6602ca6e2820ec98cc68909fdd9f87c7bd23b62000/ASIM/1/10")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('coin2asset_fee_config', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/wallet/coin2asset_fee_config")
+		let result = await rp("https://api.appdev.fingo.com/wallet/coin2asset_fee_config")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('list_fingo_config', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/wallet/list_fingo_config")
+		let result = await rp("https://api.appdev.fingo.com/wallet/list_fingo_config")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('my_express_records', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/express/my_records/0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9/1/3")
+		let result = await rp("https://api.appdev.fingo.com/express/my_records/0x6632bd37c1331b34359920f1eaa18a38ba9ff203e9/1/3")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('get_express_trade', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/express/get_express_trade/4e6b881de2eb3b9e8bdb4baefac9d5182c54eb274c821ca43e04301c9a7e2497")
+		let result = await rp("https://api.appdev.fingo.com/express/get_express_trade/4e6b881de2eb3b9e8bdb4baefac9d5182c54eb274c821ca43e04301c9a7e2497")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 
 	it('express_config', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/express/config")
+		let result = await rp("https://api.appdev.fingo.com/express/config")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 
 	it('get_express_price', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/express/get_price/ASIM/CNYC/1")
+		let result = await rp("https://api.appdev.fingo.com/express/get_price/ASIM/CNYC/1")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 
 	it('get_pool_info', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/express/get_pool_info")
+		let result = await rp("https://api.appdev.fingo.com/express/get_pool_info")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
@@ -281,11 +281,11 @@ describe('API', function() {
 /*
 	it('build_express', async() =>{
 		const start = new Date().getTime()
-		let result = await rp("http://127.0.0.1:21000/express/")
+		let result = await rp("https://api.appdev.fingo.com/express/")
 		const end = new Date().getTime()
 		result = JSON.parse(result);
 		expect(result.success).to.be.equal(true);
-		expect(end-start).to.be.lessThan(300)
+		expect(end-start).to.be.lessThan(3000)
 		
 	});
 */
