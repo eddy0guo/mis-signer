ba03bedf50373571ce973153a0d8a6edd4fde012
ryan <ryan@imac.sh>
2020-02-19 13:57:32 +0800

    NP & BN lib float test

---
+++ src/test/test_float.ts
@@ -0,0 +1,10 @@
+import NP from 'number-precision';
+import BigNumber from 'bignumber.js';
+
+let a:BigNumber = new BigNumber(0.33);
+a = a.times(103343.6081)
+a = a.times(100000000)
+
+console.log('BN',a.toFixed());
+
+console.log('NP',NP.round(NP.times(0.33,103343.6081,100000000),0));
